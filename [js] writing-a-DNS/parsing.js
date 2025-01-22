4bf2337d98228ee45fd8dc717096042706f4d7db
Sentinel <muhamad.salman2001@gmail.com>
2025-01-22 20:47:51 +0100

    added RR parsing function

--- [js] writing-a-DNS/parsing.js
+++ [js] writing-a-DNS/parsing.js
@@ -1,5 +1,5 @@
-import fs from "fs";
-import readline from "readline";
+const fs = require("fs");
+const readline = require("readline");
 
 async function processBindFile(filePath = "zones/example2.com.zone") {
   // creating readline interface
@@ -116,7 +116,7 @@ async function processBindFile(filePath = "zones/example2.com.zone") {
             default:
               break;
           }
-          if (lineSplit.includes( ")" )) {
+          if (lineSplit.includes(")")) {
             multiLineSoa = false;
           }
           soaLine++;
@@ -125,5 +125,91 @@ async function processBindFile(filePath = "zones/example2.com.zone") {
     } //end: if line > 1
   } // end: for
 
-  return records; 
+  return records;
 }
+
+// process resource record
+function processRr(splittedLine, containsTtl, previousTtl, 
+  previousName, origin, ttl) {
+
+  let rr = {};
+  let totalLength = splittedLine.length;
+  let isMx = Number(splittedLine[totalLength - 2]);
+
+  switch (totalLength) {
+    case 5:
+      for (let index = 0; index < totalLength; index++) {
+        const element = splittedLine[index];
+        if (!element.includes(".")) {
+          if (parseInt(element)) {
+            if (!isMx) {
+              containsTtl = true;
+              previousTtl = element;
+              splittedLine.splice(index, 1);
+            }
+            break;
+          }
+        }
+      }
+
+      if (!isMx) {
+        previousName = splittedLine[0];
+        rr.class = splittedLine[1];
+        rr.type = splittedLine[2];
+        rr.data = splittedLine[3];
+      }
+
+      break;
+    case 4:
+      for (let index = 0; index < totalLength; index++) {
+        const element = splittedLine[index];
+        if (!element.includes(".")) {
+          if (parseInt(element)) {
+            if (!isMx) {
+              containsTtl = true;
+              previousTtl = element;
+              splittedLine.splice(index, 1);
+            }
+            break;
+          }
+        }
+      }
+
+      if (containsTtl) {
+        //Name is missing
+        rr.class = splittedLine[0];
+        rr.type = splittedLine[1];
+        rr.data = splittedLine[2];
+      } else {
+        if (isMx) {
+          previousName = "@";
+          rr.class = splittedLine[0];
+          rr.type = splittedLine[1];
+          rr.preference = splittedLine[2];
+          rr.data = splittedLine[3];
+        } else {
+          previousName = splittedLine[0];
+          rr.class = splittedLine[1];
+          rr.type = splittedLine[2];
+          rr.data = splittedLine[3];
+        }
+      }
+
+      break;
+    case 3:
+      rr.class = splittedLine[0];
+      rr.type = splittedLine[1];
+      rr.data = splittedLine[2];
+      break;
+    case 2:
+      break;
+    default:
+      break;
+  }
+  rr.name = previousName || origin;
+  rr.ttl = previousTtl || ttl;
+
+  return [rr, previousName, previousTtl];
+}
+
+export { processBindFile };
