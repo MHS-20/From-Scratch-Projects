0f8733eacb94acc6cfe1574975103cd2942c1299
Sentinel <muhamad.salman2001@gmail.com>
2025-01-24 14:28:15 +0100

    fixed tap setup

--- [C] writing a virtual switch for VPNs /setup.sh
+++ [C] writing a virtual switch for VPNs /setup.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
+ip tuntap add dev mytap mode tap
 ip addr add 10.1.1.101/24 dev mytap
 ip link set mytap up
-
-gcc -I. -c tap_utils.c -o tap_utils.o
-gcc -I. -c vPort.c -o vPort.o
-gcc -o vport vPort.o tap_utils.o -lpthread
-
-gcc -c vSwitch.c -o vSwitch.o
-gcc -o vswitch vSwitch.o
